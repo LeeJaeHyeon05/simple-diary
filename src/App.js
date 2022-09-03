2ea8cf2adf6713e21baeb84b217527ae9bfd4065
jh <jaehyeon0825@gmail.com>
2022-09-04 01:10:38 +0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Dairy from './Dairy';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Dairy />
+      </header>
+    </div>
+  );
+}
+
+export default App;
